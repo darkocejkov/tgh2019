97114dfaf099d458837db1456b098b9df761ef2e
cybersaint <cejk5590@mylaurier.ca>
2019-10-05 20:27:40 -0400

    Created django-based website
    still needs load static and DB integration

---
+++ site/insuredatasafe/pages/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index_view(request, *args, **kwargs):
+    return render(request, 'index.html', {})
+
+def about_view(request, *args, **kwargs):
+    return render(request, 'about.html', {})
